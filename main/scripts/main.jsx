94ecd7a868fe29cadb105d9394538d745f394a21
Adrian Horning <m.adrian.horning@gmail.com>
2018-04-13 15:48:14 -0700

    Adding express

--- main/scripts/main.jsx
+++ main/scripts/main.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      thisThat: null
+    };
+  }
 
+  componentDidMount() {
+    // const request = async () => {
+    //   const response = await fetch('http://itsthisforthat.com/api.php?json', {
+    //   });
+    //   const json = await response.json();
+    //   console.log(json);
+    // }
+    // request();
+  }
   render() {
-    return 'Hey'
+    if (this.state.thisThat) {
+      return this.state.thisThat;
+    } else {
+      return null;
+    }
   }
 }
 
